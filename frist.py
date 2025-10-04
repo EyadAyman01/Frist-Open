e67f34d9f3b9ec5fab73218c7cd8ff2b31218cef
EyadAyman01 <eyad.ayman.moh11@gmail.com>
2025-10-04 17:16:52 +0300

    Add files via upload

---
+++ frist.py
@@ -0,0 +1,16 @@
+item1 = 50
+item2 = 50
+item3 = 100
+budget = 200
+total_cost = item1 + item2 + item3
+print("budget:" ,budget)
+print("total_cost:" ,total_cost)
+
+shipsy = 10
+pepsy = 20
+twist = 20
+cheese = 250
+budget = 400
+total_cost = shipsy + pepsy + twist + cheese
+print("total_cost:", total_cost)
+print("budget:" ,budget)
